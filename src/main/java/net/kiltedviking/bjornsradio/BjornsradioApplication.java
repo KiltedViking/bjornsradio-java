8e3ac7cf29b4f2b9c6eb91e34d63c3e676456899
Björn G D Persson <bjorn@kiltedviking.net>
2025-06-21 11:04:36 +0200

    first commit

---
+++ src/main/java/net/kiltedviking/bjornsradio/BjornsradioApplication.java
@@ -0,0 +1,13 @@
+package net.kiltedviking.bjornsradio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjornsradioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjornsradioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kiltedviking/bjornsradio/BjornsradioApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kiltedviking.bjornsradio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjornsradioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
